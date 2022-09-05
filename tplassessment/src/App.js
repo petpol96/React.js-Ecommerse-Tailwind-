3dc989ec1d6b7ad086baac5f1820a36ec9ca8597
ppolitis <petpolitis@outlook.com>
2022-09-06 01:30:11 +0300

    wrong install

--- tplassessment/src/App.js
+++ tplassessment/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from './LandingPage/LandingPage';
+import Spa from './SPA/Spa';
 
 function App() {
   return (
-    <LandingPage/>
-  );
+    <Spa/>
+    )
 }
 
 export default App;
---
+++ tplassessment/src/SPA/Spa.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SpaHeader from './SpaHeader'
+
+function Spa() {
+  return (
+    <div style={{width:'100%',height:'100px'}}>
+
+        <SpaHeader/>
+    </div>
+  )
+}
+
+export default Spa
